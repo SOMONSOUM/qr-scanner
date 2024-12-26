8e151a48df6cc016b65d8365a8d1d76620489cb6
SOUM SOM ON <somon.soum9@gmail.com>
2024-12-26 16:18:36 +0700

    setup project

---
+++ src/app/scan/page.tsx
@@ -0,0 +1,9 @@
+import { ScanQRScreen } from "@/screens/scan";
+
+export default function Scan() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <ScanQRScreen />
+    </main>
+  );
+}
---
+++ src/screens/scan/index.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { QRScanner } from "./components/qr-scanner";
+
+export const ScanQRScreen = () => {
+  return <QRScanner />;
+};
